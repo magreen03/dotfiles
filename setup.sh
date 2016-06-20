9a599628d122828da18e7f7f3ac5531c4f2820e0
magreen03 <michael.green@axial.net>
2016-06-20 17:40:13 -0400

    adds a setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Setting up Symlink files"
+ln -s ~/.vimrc ~/dotfiles/vimrc
+ln -s ~/.tmux.conf ~/dotfiles/tmux.conf
+ln -s ~/.bash_profile ~/dotfiles/bash_profile
+ln -s ~/.bash_prompt ~/dotfiles/bash_prompt
+ln -s ~/.bashrc ~/dotfiles/bashrc
+
